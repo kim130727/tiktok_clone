f5760fd560fb1203364fe504acde9403b19cb466
kim130727 <kim130727@gmail.com>
2023-09-01 15:43:23 +0900

    3.1 Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-///0
+//main.dart
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TiktokApp());
@@ -14,7 +16,16 @@ class TiktokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A), //틱톡 시그니쳐 컬러
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(children: [
+            Text('Hello'),
+            Gaps.h20, //size가 20인 가로 공간을 부여, utility class로 단축해서 활용가능
+            Text('Hello2'),
+          ]),
+        ),
+      ),
     );
   }
 }
