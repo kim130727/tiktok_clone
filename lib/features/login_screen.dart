424d6f97a3081829ca505b10fb8eea523d009f74
kim130727 <kim130727@gmail.com>
2023-09-13 16:21:33 +0900

    4.2 AuthButton

--- lib/features/login_screen.dart
+++ lib/features/login_screen.dart
@@ -1,8 +1,10 @@
 //login_screen.dart
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/sign_up_screen.dart';
+import 'package:tiktok_clone/features/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -42,6 +44,18 @@ class LoginScreen extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
+              Gaps.v40,
+              AuthButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.user,
+                  ),
+                  text: "Use email & password"),
+              Gaps.v16,
+              AuthButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.apple,
+                  ),
+                  text: "Continue with Apple"),
             ],
           ),
         ),
--- lib/features/sign_up_screen.dart
+++ lib/features/sign_up_screen.dart
@@ -1,5 +1,6 @@
 //sign_up_screen.dart
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/login_screen.dart';
@@ -44,8 +45,17 @@ class SignUpScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Gaps.v40,
-              AuthButton(text: "Use phone or email"),
-              AuthButton(text: "Continue with Apple"),
+              AuthButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.user,
+                  ),
+                  text: "Use email & password"),
+              Gaps.v16,
+              AuthButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.apple,
+                  ),
+                  text: "Continue with Apple"),
             ],
           ),
         ),
