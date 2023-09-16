4da836b82d6cde17498372f5372c41ad9cd3c060
kim130727 <kim130727@gmail.com>
2023-09-16 16:51:21 +0900

    4.3 Sign Up Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //main.dart
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/sign_up_screen.dart';
 
 void main() {
@@ -13,8 +14,17 @@ class TiktokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(
-        primaryColor: const Color(0xFFE9435A), //틱톡 시그니쳐 컬러
-      ),
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFE9435A), //틱톡 시그니쳐 컬러
+          appBarTheme: const AppBarTheme(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              elevation: 0,
+              titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: Sizes.size16 + Sizes.size2,
+                fontWeight: FontWeight.w600,
+              ))),
       home: const SignUpScreen(),
     );
   }
