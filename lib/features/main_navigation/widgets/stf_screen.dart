c3a1643993bc0b5a1c5ad8d078e0485deeecf3fb
kim130727 <kim130727@gmail.com>
2023-11-07 18:23:57 +0900

    6.7 Stateful Navigation part Two

--- lib/features/main_navigation/widgets/stf_screen.dart
+++ lib/features/main_navigation/widgets/stf_screen.dart
@@ -26,6 +26,7 @@ class _StfScreenState extends State<StfScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print('im built!');
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
