55fa9ca5fc7e06907d085598d062a7da0bddcec5
kim130727 <kim130727@gmail.com>
2023-11-13 20:50:04 +0900

    7.1 Infinite Scrolling

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -10,20 +10,33 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  List<Color> colors = [Colors.blue, Colors.red, Colors.yellow, Colors.teal];
+
+  void _onPageChanged(int page) {
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([Colors.blue, Colors.red, Colors.yellow, Colors.teal]);
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(
-          color: Colors.blue,
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container(
+        color: colors[index],
+        child: Center(
+          child: Text(
+            "Screen $index",
+            style: const TextStyle(fontSize: 68),
+          ),
         ),
-        Container(
-          color: Colors.teal,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-      ],
+      ),
     );
   }
 }
