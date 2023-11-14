2f6f3ac336d3ed48c85bf9306433b8437dd40045
kim130727 <kim130727@gmail.com>
2023-11-14 19:57:53 +0900

    7.2 PageController

---
+++ lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,16 @@
+//video_post.dart
+import 'package:flutter/cupertino.dart';
+
+class VideoPost extends StatefulWidget {
+  const VideoPost({super.key});
+
+  @override
+  State<VideoPost> createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
