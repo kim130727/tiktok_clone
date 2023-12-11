f5ab706c6b5f6df7450699124d11742550ed76c7
kim130727 <kim130727@gmail.com>
2023-12-11 15:11:49 +0900

    9.1 Light Navigation

---
+++ lib/features/discover/discover_screen.dart
@@ -0,0 +1,16 @@
+//discover_screen.dart
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Discover'),
+      ),
+    );
+  }
+}
