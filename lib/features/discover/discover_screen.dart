ee8f1c5111d500274fea2dd60567b295603e88c4
kim130727 <kim130727@gmail.com>
2023-12-19 15:30:42 +0900

    9.4 Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,6 +1,8 @@
 //discover_screen.dart
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -48,23 +50,78 @@ class DiscoverScreen extends StatelessWidget {
         body: TabBarView(
           children: [
             GridView.builder(
-                itemCount: 20, //지정해주지 않으면 무한으로 나옴
-                padding: const EdgeInsets.all(
-                  Sizes.size1,
-                ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  //몇개의 Column을 가질지 정하도록 해줌
-                  crossAxisCount: 2, //컬럼의 개수
-                  crossAxisSpacing: Sizes.size10, //컬럼간 좌우 간격
-                  mainAxisSpacing: Sizes.size10, //컬럼간 위아래 간격
-                  childAspectRatio: 9 / 16, //컬럼 세로가로 비율
-                ),
-                itemBuilder: (context, index) => Container(
-                      color: Colors.teal,
-                      child: Center(
-                        child: Text("$index"),
-                      ),
-                    )),
+              itemCount: 20, //지정해주지 않으면 무한으로 나옴
+              padding: const EdgeInsets.all(
+                Sizes.size1,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                //몇개의 Column을 가질지 정하도록 해줌
+                crossAxisCount: 2, //컬럼의 개수
+                crossAxisSpacing: Sizes.size10, //컬럼간 좌우 간격
+                mainAxisSpacing: Sizes.size10, //컬럼간 위아래 간격
+                childAspectRatio: 9 / 22, //컬럼 세로가로 비율
+              ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover, //강제로 특정한 비율을 따르는 위젯
+                      placeholder:
+                          "C:\\flutter\\tiktok_clone\\lib\\features\\assets\\images\\image2.jpg",
+                      image:
+                          "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&Wixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    "This is a very long caption for my tiktok that im upload just now currently.",
+                    overflow: TextOverflow.ellipsis, //말줄임표 사용
+                    maxLines: 2,
+                    style: TextStyle(
+                      fontSize: Sizes.size12 + Sizes.size2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v8,
+                  DefaultTextStyle(
+                    //같은 스타일이 두 텍스트 모두에게 적용됨
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/47652911",
+                          ),
+                        ),
+                        Gaps.h4,
+                        const Expanded(
+                          child: Text(
+                            "My avatar is going to be very long",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          "2.5M",
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
             for (var tab in tabs.skip(1)) //첫번째 tab 생략
               Center(
                 child: Text(
