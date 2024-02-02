aed953c166e08029240115b25677a5e6905fe2ef
kim130727 <kim130727@gmail.com>
2024-02-02 10:16:48 +0900

    14.3 MediaQuery

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,7 +1,7 @@
-//discover_screen.dart
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -24,28 +24,29 @@ class DiscoverScreen extends StatefulWidget {
 
 class _DiscoverScreenState extends State<DiscoverScreen> {
   final TextEditingController _textEditingController =
-      TextEditingController(text: "Initial Text"); //search form 자동완성
+      TextEditingController(text: "Initial Text");
 
   void _onSearchChanged(String value) {
     print("Searching form $value");
-  } //Search 문구 출력
+  }
 
   void _onSearchSubmitted(String value) {
     print("Submitted $value");
-  } //버튼 누르면 출력
+  }
 
   @override
   void dispose() {
     _textEditingController.dispose();
-    super.dispose(); //controller는 꼭 dispose 해야 함
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
-        resizeToAvoidBottomInset: false, //키보드가 나오면 scaffold resize 조절 안하게 조정
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1,
           title: CupertinoSearchTextField(
@@ -70,58 +71,53 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
               for (var tab in tabs)
                 Tab(
                   text: tab,
-                )
+                ),
             ],
           ),
         ),
         body: TabBarView(
           children: [
             GridView.builder(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior
-                  .onDrag, //GridView를 드래그할 때마다 keyboard가 자동으로 사라짐
-              itemCount: 20, //지정해주지 않으면 무한으로 나옴
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              itemCount: 20,
               padding: const EdgeInsets.all(
-                Sizes.size1,
+                Sizes.size10,
               ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                //몇개의 Column을 가질지 정하도록 해줌
-                crossAxisCount: 2, //컬럼의 개수
-                crossAxisSpacing: Sizes.size10, //컬럼간 좌우 간격
-                mainAxisSpacing: Sizes.size10, //컬럼간 위아래 간격
-                childAspectRatio: 9 / 22, //컬럼 세로가로 비율
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: width > Breakpoints.lg ? 5 : 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 20,
               ),
               itemBuilder: (context, index) => Column(
                 children: [
                   Container(
-                    clipBehavior: Clip.hardEdge, //이미지가 overflow 되는 것을 조정
+                    clipBehavior: Clip.hardEdge,
                     decoration: BoxDecoration(
-                      //이미지 테두리를 동그랗게
                       borderRadius: BorderRadius.circular(Sizes.size4),
                     ),
                     child: AspectRatio(
                       aspectRatio: 9 / 16,
                       child: FadeInImage.assetNetwork(
-                        fit: BoxFit.cover, //강제로 특정한 비율을 따르는 위젯
-                        placeholder:
-                            "C:\\flutter\\tiktok_clone\\lib\\features\\assets\\images\\image2.jpg",
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/placeholder.jpg",
                         image:
-                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&Wixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
                       ),
                     ),
                   ),
                   Gaps.v10,
                   const Text(
                     "This is a very long caption for my tiktok that im upload just now currently.",
-                    overflow: TextOverflow.ellipsis, //말줄임표 사용
+                    overflow: TextOverflow.ellipsis,
                     maxLines: 2,
                     style: TextStyle(
-                      fontSize: Sizes.size12 + Sizes.size2,
+                      fontSize: Sizes.size16 + Sizes.size2,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Gaps.v8,
                   DefaultTextStyle(
-                    //같은 스타일이 두 텍스트 모두에게 적용됨
                     style: TextStyle(
                       color: Colors.grey.shade600,
                       fontWeight: FontWeight.w600,
@@ -131,7 +127,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                         const CircleAvatar(
                           radius: 12,
                           backgroundImage: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/47652911",
+                            "https://avatars.githubusercontent.com/u/3612017",
                           ),
                         ),
                         Gaps.h4,
@@ -158,7 +154,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 ],
               ),
             ),
-            for (var tab in tabs.skip(1)) //첫번째 tab 생략
+            for (var tab in tabs.skip(1))
               Center(
                 child: Text(
                   tab,
