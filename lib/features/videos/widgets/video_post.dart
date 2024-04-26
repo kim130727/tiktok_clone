7542dc6b745a7ae49da5a588c6572155123faa8a
kim130727 <kim130727@gmail.com>
2024-04-26 17:01:02 +0900

    20.9 ChangeNotifier

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:marquee/marquee.dart';
 import 'package:tiktok_clone/common/widgets/video_config/video_config.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -13,25 +12,16 @@ import 'package:tiktok_clone/generated/l10n.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
-const keywords = [
-  "tag1",
-  "tag2",
-  "tag3",
-  "tag4",
-  "tag5",
-  "tag6",
-  "tag7",
-  "tag8",
-  "tag9",
-];
-
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
 
   final int index;
 
-  const VideoPost(
-      {super.key, required this.onVideoFinished, required this.index});
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -39,141 +29,135 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost>
     with SingleTickerProviderStateMixin {
-  late final VideoPlayerController videoPlayerController;
-  Duration animationDuration = const Duration(milliseconds: 200);
+  late final VideoPlayerController _videoPlayerController;
 
-  late final AnimationController animationController;
+  final Duration _animationDuration = const Duration(milliseconds: 200);
 
-  bool isPaused = false;
-  bool _isMoreTagsShowed = false;
+  late final AnimationController _animationController;
 
-  final Iterable<String> _tags = keywords.map((tag) => "#$tag");
-  late final String _tagString;
+  bool _isPaused = false;
 
-  void onVideoChange() {
-    if (videoPlayerController.value.isInitialized) {
-      if (videoPlayerController.value.duration ==
-          videoPlayerController.value.position) {
+  bool _autoMute = videoConfig.autoMute;
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
         widget.onVideoFinished();
       }
     }
   }
 
-  void initVideoPlayer() async {
-    videoPlayerController =
-        VideoPlayerController.asset("lib/features/assets/videos/0003.mp4");
-    await videoPlayerController.initialize();
-    await videoPlayerController.setLooping(true);
+  void _initVideoPlayer() async {
+    _videoPlayerController =
+        VideoPlayerController.asset("assets/videos/video.mp4");
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
     if (kIsWeb) {
-      await videoPlayerController.setVolume(0);
-    } //future를 return함
-    videoPlayerController.addListener(onVideoChange);
+      await _videoPlayerController.setVolume(0);
+    }
+    _videoPlayerController.addListener(_onVideoChange);
     setState(() {});
-  }
-
-  void _onSeeMoreClick() {
-    setState(() {
-      _isMoreTagsShowed = !_isMoreTagsShowed;
-    });
   }
 
   @override
   void initState() {
     super.initState();
-    initVideoPlayer();
+    _initVideoPlayer();
 
-    _tagString = _tags.reduce((value, element) => "$value $element");
-    print(_tagString);
-
-    animationController = AnimationController(
+    _animationController = AnimationController(
       vsync: this,
       lowerBound: 1.0,
       upperBound: 1.5,
       value: 1.5,
-      duration: animationDuration,
+      duration: _animationDuration,
     );
+
+    videoConfig.addListener(() {
+      setState(() {
+        _autoMute = videoConfig.autoMute;
+      });
+    });
   }
 
   @override
   void dispose() {
-    videoPlayerController.dispose();
-    animationController.dispose();
+    _videoPlayerController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
-  void onVisibilityChanged(VisibilityInfo info) {
+  void _onVisibilityChanged(VisibilityInfo info) {
     if (!mounted) return;
     if (info.visibleFraction == 1 &&
-        !isPaused &&
-        !videoPlayerController.value.isPlaying) {
-      videoPlayerController.play();
+        !_isPaused &&
+        !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
     }
-    if (videoPlayerController.value.isPlaying && info.visibleFraction == 0) {
-      onTogglePause();
+    if (_videoPlayerController.value.isPlaying && info.visibleFraction == 0) {
+      _onTogglePause();
     }
   }
-  //영상의 visibility가 변하고 영상이 전부 화면에 들어 있으면서 재생이 안되고 있다면 재생하도록 함
 
-  void onTogglePause() {
-    if (videoPlayerController.value.isPlaying) {
-      videoPlayerController.pause();
-      animationController.reverse();
+  void _onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
     } else {
-      videoPlayerController.play();
-      animationController.forward();
+      _videoPlayerController.play();
+      _animationController.forward();
     }
     setState(() {
-      isPaused = !isPaused;
+      _isPaused = !_isPaused;
     });
   }
 
   void _onCommentsTap(BuildContext context) async {
-    if (videoPlayerController.value.isPlaying) {
-      onTogglePause();
-    } //댓글 버튼을 누르면 pause가 되는 함수
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
     await showModalBottomSheet(
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.transparent,
       builder: (context) => const VideoComments(),
     );
-    print('closed'); //await로 인해 댓글창을 닫아야 작동됨
-    onTogglePause();
+    _onTogglePause();
   }
 
   @override
   Widget build(BuildContext context) {
     return VisibilityDetector(
       key: Key("${widget.index}"),
-      onVisibilityChanged: onVisibilityChanged,
+      onVisibilityChanged: _onVisibilityChanged,
       child: Stack(
         children: [
           Positioned.fill(
-            child: videoPlayerController.value.isInitialized
-                ? VideoPlayer(videoPlayerController)
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
                 : Container(
                     color: Colors.black,
                   ),
           ),
           Positioned.fill(
             child: GestureDetector(
-              onTap: onTogglePause,
+              onTap: _onTogglePause,
             ),
           ),
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
                 child: AnimatedBuilder(
-                  animation: animationController,
+                  animation: _animationController,
                   builder: (context, child) {
                     return Transform.scale(
-                      scale: animationController.value,
+                      scale: _animationController.value,
                       child: child,
                     );
                   },
                   child: AnimatedOpacity(
-                    opacity: isPaused ? 1 : 0,
-                    duration: animationDuration,
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
                     child: const FaIcon(
                       FontAwesomeIcons.play,
                       color: Colors.white,
@@ -189,22 +173,22 @@ class _VideoPostState extends State<VideoPost>
             top: 40,
             child: IconButton(
               icon: FaIcon(
-                VideoConfigData.of(context).autoMute
+                _autoMute
                     ? FontAwesomeIcons.volumeOff
                     : FontAwesomeIcons.volumeHigh,
                 color: Colors.white,
               ),
-              onPressed: VideoConfigData.of(context).toggleMuted,
+              onPressed: videoConfig.toggleAutoMute,
             ),
           ),
-          Positioned(
+          const Positioned(
             bottom: 20,
             left: 10,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  "@지율댄스",
+                Text(
+                  "@니꼬",
                   style: TextStyle(
                     fontSize: Sizes.size20,
                     color: Colors.white,
@@ -212,90 +196,13 @@ class _VideoPostState extends State<VideoPost>
                   ),
                 ),
                 Gaps.v10,
-                const Text(
-                  "지율이가 여름성경학교에서 찬양춤 보여줬다",
+                Text(
+                  "This is my house in Thailand!!!",
                   style: TextStyle(
                     fontSize: Sizes.size16,
                     color: Colors.white,
                   ),
-                ),
-                Gaps.v10,
-                SizedBox(
-                  width: 300,
-                  child: _isMoreTagsShowed
-                      ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              _tagString,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: Sizes.size16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: _onSeeMoreClick,
-                              child: const Text(
-                                "Folded",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: Sizes.size16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            )
-                          ],
-                        )
-                      : Row(
-                          children: [
-                            Flexible(
-                              child: Text(
-                                _tagString,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: Sizes.size16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: _onSeeMoreClick,
-                              child: const Text(
-                                "See more",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: Sizes.size16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                ),
-                Gaps.v10,
-                Row(
-                  children: [
-                    const FaIcon(
-                      FontAwesomeIcons.music,
-                      size: 16,
-                      color: Colors.white,
-                    ),
-                    Gaps.h8,
-                    SizedBox(
-                      width: 200,
-                      height: 16,
-                      child: Marquee(
-                          text:
-                              "This text is to long to be shown in just one line",
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: Sizes.size16,
-                          )),
-                    )
-                  ],
-                ),
+                )
               ],
             ),
           ),
@@ -309,8 +216,9 @@ class _VideoPostState extends State<VideoPost>
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
                   foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/47652911"),
-                  child: Text("나율"),
+                    "https://avatars.githubusercontent.com/u/3612017",
+                  ),
+                  child: Text("니꼬"),
                 ),
                 Gaps.v24,
                 VideoButton(
@@ -318,13 +226,17 @@ class _VideoPostState extends State<VideoPost>
                     text: S.of(context).likeCount(98798711111987)),
                 Gaps.v24,
                 GestureDetector(
-                    onTap: () => _onCommentsTap(context),
-                    child: VideoButton(
-                      icon: FontAwesomeIcons.solidComment,
-                      text: S.of(context).commentCount(65656),
-                    )),
+                  onTap: () => _onCommentsTap(context),
+                  child: VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: S.of(context).commentCount(65656),
+                  ),
+                ),
                 Gaps.v24,
-                const VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
+                const VideoButton(
+                  icon: FontAwesomeIcons.share,
+                  text: "Share",
+                )
               ],
             ),
           ),
